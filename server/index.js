669092ebeef720e73f076aad32755745bf788b74
Kit Jonathan Te <kjonathante@gmail.com>
2019-04-27 19:00:21 -0700

    Added cors functionality to express

--- server/index.js
+++ server/index.js
@@ -4,7 +4,11 @@ const app = express()
 
 app.use(express.json())
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/messages', async (req, res) => {
   const messages = await prisma.messages()
