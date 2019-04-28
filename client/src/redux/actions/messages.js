86cf8e7ba84c5d2b3abac02990f4fc0551212b93
Kit Jonathan Te <kjonathante@gmail.com>
2019-04-28 09:54:48 -0700

    Changed the State Shape. {   status: '...',   allIds: [...],   byIds: {     [id]: {       ...     }   } } Added a receiveMessages function to handle result from fetch.

--- client/src/redux/actions/messages.js
+++ client/src/redux/actions/messages.js
@@ -7,6 +7,22 @@ import {
   ERROR
 } from "./ActionTypes";
 
+const receiveMessages = (messages) => {
+  let allIds = []
+  let byIds = {}
+  messages.forEach( message => {
+    allIds[allIds.length]= message.id
+    byIds[message.id] = { message: message.message }
+  })
+  return {
+    type: SUCCESS,
+    payload: {
+      byIds,
+      allIds
+    }
+  }
+}
+
 export const getMessages = async (dispatch, messagesService) => {
   //Set the applications to a "Loading" state
   dispatch({ type: REQUEST });
@@ -22,11 +38,7 @@ export const getMessages = async (dispatch, messagesService) => {
     const messages = await response.json();
     const isMessagesEmpty = messages.length === 0;
     if (isMessagesEmpty) dispatch({ type: EMPTY });
-    else
-      dispatch({
-        type: SUCCESS,
-        messages
-      });
+    else dispatch( receiveMessages(messages) );
   } catch (error) {
     const isError404 = error.response && error.response.status === 404;
     if (isError404) dispatch({ type: NOT_FOUND });
--- client/src/redux/reducers/messages.js
+++ client/src/redux/reducers/messages.js
@@ -1,14 +1,17 @@
 import { SUCCESS } from "../actions/ActionTypes";
 
 const initialState = {
-  messages: [],
-  status: null
+  status: null,
+  allIds: [],
+  byIds: {}
 };
 
 export default function messages(state = initialState, action) {
   switch (action.type) {
-    case SUCCESS:
-      return { ...state, messages: action.messages, status: action.type };
+    case SUCCESS: {
+      const {byIds, allIds} = action.payload
+      return { ...state, byIds, allIds, status: action.type };
+    }
     default:
       return { ...state, status: action.type };
   }
